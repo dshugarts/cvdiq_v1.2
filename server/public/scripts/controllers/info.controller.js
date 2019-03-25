21e3c2023e4ba15f6f5bd084f60b4bca15f946df
Ariaga <daniel.shugarts@gmail.com>
2019-03-24 22:58:26 -0500

    fixed name in nav bar for about page

--- server/public/scripts/controllers/info.controller.js
+++ server/public/scripts/controllers/info.controller.js
@@ -1,5 +1,6 @@
-myApp.controller('InfoController', ['UserService', function(UserService) {
+myApp.controller('InfoController', ['$http', '$location', 'UserService', function($http, $location, UserService) {
   // console.log('InfoController created');
   var self = this;
   self.userService = UserService;
+  self.userObject = UserService.userObject;
 }]);
